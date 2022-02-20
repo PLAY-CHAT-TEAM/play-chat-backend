4d7c14a064549128438edd66aa88f80d48a3c8d2
khcho902 <khcho902@gmail.com>
2022-02-20 16:30:00 +0900

    init project

---
+++ src/main/java/me/kycho/playchat/PlayChatApplication.java
@@ -0,0 +1,13 @@
+package me.kycho.playchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kycho/playchat/PlayChatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kycho.playchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
