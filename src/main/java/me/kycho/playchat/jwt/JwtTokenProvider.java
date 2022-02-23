c918d0f15ea063394a3efd670709b4a5dc577278
khcho902 <khcho902@gmail.com>
2022-02-24 01:26:20 +0900

    feat: JWT생성과 유효성 검증을 수행하는 JwtTokenProvider 추가

---
+++ src/main/java/me/kycho/playchat/jwt/JwtTokenProvider.java
@@ -0,0 +1,94 @@
+package me.kycho.playchat.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final Key key;
+    private final long tokenValidityInMilliseconds;
+
+    public JwtTokenProvider(
+        @Value("${jwt.secret}") String secret,
+        @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds
+    ) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+    }
+
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+            .setSubject(authentication.getName())
+            .claim(AUTHORITIES_KEY, authorities)
+            .signWith(key, SignatureAlgorithm.HS512)
+            .setExpiration(validity)
+            .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+            Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
---
+++ src/test/java/me/kycho/playchat/jwt/JwtTokenProviderTest.java
@@ -0,0 +1,68 @@
+package me.kycho.playchat.jwt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+class JwtTokenProviderTest {
+
+    @Test
+    @DisplayName("JWT 토큰 생성 및 authentication 조회 테스트")
+    void createTokenTest_And_getAuthenticationTest() {
+        // given
+        String email = "kycho@naver.com";
+        List<GrantedAuthority> authorities = Stream.of("MEMBER", "ADMIN")
+            .map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+        Authentication authentication =
+            new UsernamePasswordAuthenticationToken(email, "", authorities);
+
+        JwtTokenProvider tokenProvider = new JwtTokenProvider(
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            86400L);
+
+        // when
+        String token = tokenProvider.createToken(authentication);
+        Authentication result = tokenProvider.getAuthentication(token);
+
+        // then
+        assertThat(result.getName()).isEqualTo(email);
+        assertThat(result.getAuthorities()).isEqualTo(authorities);
+    }
+
+    @Test
+    @DisplayName("토큰 유효성 검사 테스트")
+    void validateTokenTest() {
+        // given
+        String email = "kycho@naver.com";
+        List<GrantedAuthority> authorities = Stream.of("MEMBER", "ADMIN")
+            .map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+        Authentication authentication =
+            new UsernamePasswordAuthenticationToken(email, "", authorities);
+
+        JwtTokenProvider tokenProvider = new JwtTokenProvider(
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            86400L);
+        JwtTokenProvider tokenProviderNoTime = new JwtTokenProvider(
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            0L);
+
+        // when
+        String token = tokenProvider.createToken(authentication);
+        String tokenNoTime = tokenProviderNoTime.createToken(authentication);
+
+        // then
+        assertThat(tokenProvider.validateToken(token)).isTrue();
+        assertThat(tokenProvider.validateToken(token + "a")).isFalse();
+        assertThat(tokenProvider.validateToken(tokenNoTime)).isFalse();
+    }
+}
