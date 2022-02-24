a6500f856579084ccc4aebdc97d83d9d84aeb586
khcho902 <khcho902@gmail.com>
2022-02-24 17:20:25 +0900

    feat: jwt인증에 사용 할 security filter 추가

--- src/main/java/me/kycho/playchat/config/SecurityConfig.java
+++ src/main/java/me/kycho/playchat/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package me.kycho.playchat.config;
 
+import lombok.RequiredArgsConstructor;
+import me.kycho.playchat.jwt.JwtAuthenticationFilter;
+import me.kycho.playchat.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider tokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,10 +27,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .authorizeRequests()
             .antMatchers("/api/members/join").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(new JwtAuthenticationFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class)
         ;
     }
 }
