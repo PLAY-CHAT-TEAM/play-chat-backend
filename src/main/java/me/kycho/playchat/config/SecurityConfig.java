3c9cc5cea4b37871091cecab322981b954229838
khcho902 <khcho902@gmail.com>
2022-02-25 19:05:38 +0900

    feat: api문서 요청 security filter거치지 않도록 함

--- src/main/java/me/kycho/playchat/config/SecurityConfig.java
+++ src/main/java/me/kycho/playchat/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import me.kycho.playchat.security.jwt.JwtAuthenticationFilter;
 import me.kycho.playchat.security.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/docs/index.html");
     }
 
     @Override
