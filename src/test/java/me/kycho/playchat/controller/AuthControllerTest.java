89af9deec39d138f5194023d28242b0db1f3980c
khcho902 <khcho902@gmail.com>
2022-02-27 01:08:28 +0900

    docs: 로그인 api 문서화(Spring Rest Docs)

--- src/test/java/me/kycho/playchat/controller/AuthControllerTest.java
+++ src/test/java/me/kycho/playchat/controller/AuthControllerTest.java
@@ -1,5 +1,11 @@
 package me.kycho.playchat.controller;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -12,8 +18,10 @@ import me.kycho.playchat.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +30,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@AutoConfigureRestDocs
 class AuthControllerTest {
 
     @Autowired
@@ -41,7 +50,7 @@ class AuthControllerTest {
     void authenticate() throws Exception {
 
         // given
-        String email = "kycho@naver.com";
+        String email = "member@naver.com";
         String password = "password";
 
         Member member = Member.builder()
@@ -56,14 +65,30 @@ class AuthControllerTest {
 
         // when & then
         mockMvc.perform(
-            post("/api/authenticate")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(signInRequestDto))
-        )
+                post("/api/authenticate")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(signInRequestDto))
+            )
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("token").exists())
-        ;
+            .andDo(
+                document("member-login",
+                    requestHeaders(
+                        headerWithName(HttpHeaders.CONTENT_TYPE)
+                            .description("요청 메시지의 콘텐츠 타입 +" + "\n" + MediaType.APPLICATION_JSON),
+                        headerWithName(HttpHeaders.ACCEPT)
+                            .description("응답받을 콘텐츠 타입 +" + "\n" + MediaType.APPLICATION_JSON)
+                    ),
+                    requestFields(
+                        fieldWithPath("email").description("인증을 위한 이메일"),
+                        fieldWithPath("password").description("인증을 위한 비밀번호")
+                    ),
+                    responseFields(
+                        fieldWithPath("token").description("인증이 필요한 리소스 요청에 사용가능한 JWT 토큰")
+                    )
+                )
+            );
     }
 }
