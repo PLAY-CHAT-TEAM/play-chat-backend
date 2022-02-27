16d06915129e2b8d2377b662037f0fa8877b1019
khcho902 <khcho902@gmail.com>
2022-02-27 10:54:46 +0900

    feat: #9 Spring REST Docs 적용 (#17)
    * feat: spring rest docs 의존성 및 gradle 설정 추가
    
    * feat: asciidoc 샘플 추가
    
    * feat: api문서 요청 security filter거치지 않도록 함
    
    * feat: snippets속성 자동화와 operation매크로 사용을 위한 의존성 추가
    
    * docs: 회원가입 api 문서화(Spring Rest Docs)
    
    * docs: http 메시지에서 바이너리 코드 출력되는 부분 간단하게 표시
    
    * docs: index.adoc에서 불필요한 부분 제거
    
    * docs: 로그인 api 문서화(Spring Rest Docs)
    
    * docs: 로그인 결과 JWT 마스킹
    
    * docs: request, response 보기좋게 출력되도록함

--- src/test/java/me/kycho/playchat/controller/AuthControllerTest.java
+++ src/test/java/me/kycho/playchat/controller/AuthControllerTest.java
@@ -1,20 +1,36 @@
 package me.kycho.playchat.controller;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
 import me.kycho.playchat.controller.dto.SignInRequestDto;
 import me.kycho.playchat.domain.Member;
 import me.kycho.playchat.repository.MemberRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.operation.preprocess.ContentModifier;
+import org.springframework.restdocs.operation.preprocess.ContentModifyingOperationPreprocessor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +38,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@AutoConfigureRestDocs
 class AuthControllerTest {
 
     @Autowired
@@ -41,7 +58,7 @@ class AuthControllerTest {
     void authenticate() throws Exception {
 
         // given
-        String email = "kycho@naver.com";
+        String email = "member@naver.com";
         String password = "password";
 
         Member member = Member.builder()
@@ -56,14 +73,52 @@ class AuthControllerTest {
 
         // when & then
         mockMvc.perform(
-            post("/api/authenticate")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(signInRequestDto))
-        )
+                post("/api/authenticate")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(signInRequestDto))
+            )
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("token").exists())
-        ;
+            .andDo(
+                document("member-login",
+                    preprocessRequest(
+                        prettyPrint()
+                    ),
+                    preprocessResponse(
+                        new ContentModifyingOperationPreprocessor(JWTContentModifier()),
+                        prettyPrint()
+                    ),
+                    requestHeaders(
+                        headerWithName(HttpHeaders.CONTENT_TYPE)
+                            .description("요청 메시지의 콘텐츠 타입 +" + "\n" + MediaType.APPLICATION_JSON),
+                        headerWithName(HttpHeaders.ACCEPT)
+                            .description("응답받을 콘텐츠 타입 +" + "\n" + MediaType.APPLICATION_JSON)
+                    ),
+                    requestFields(
+                        fieldWithPath("email").description("인증을 위한 이메일 (필수)"),
+                        fieldWithPath("password").description("인증을 위한 비밀번호 (필수)")
+                    ),
+                    responseFields(
+                        fieldWithPath("token").description("인증이 필요한 리소스 요청에 사용가능한 JWT 토큰")
+                    )
+                )
+            );
+    }
+
+    private ContentModifier JWTContentModifier() {
+        return (originalContent, contentType) -> {
+            String ogContent = new String(originalContent);
+            try {
+                JSONObject jsonObject = new JSONObject(ogContent);
+                jsonObject.put("token", "XXXXXXX.YYYYYYYYYY.ZZZZZZ");
+                String content = jsonObject.toString();
+                return content.getBytes(StandardCharsets.UTF_8);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return "<<< ERROR >>>".getBytes(StandardCharsets.UTF_8);
+        };
     }
 }
--- src/test/java/me/kycho/playchat/controller/MemberControllerTest.java
+++ src/test/java/me/kycho/playchat/controller/MemberControllerTest.java
@@ -1,6 +1,18 @@
 package me.kycho.playchat.controller;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -8,18 +20,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import me.kycho.playchat.common.FileStore;
 import me.kycho.playchat.domain.Member;
 import me.kycho.playchat.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.operation.OperationRequest;
+import org.springframework.restdocs.operation.OperationRequestFactory;
+import org.springframework.restdocs.operation.OperationRequestPart;
+import org.springframework.restdocs.operation.OperationRequestPartFactory;
+import org.springframework.restdocs.operation.preprocess.OperationPreprocessorAdapter;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +49,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@AutoConfigureRestDocs
 class MemberControllerTest {
 
     @Autowired
@@ -70,7 +93,34 @@ class MemberControllerTest {
             .andExpect(status().isCreated())
             .andExpect(jsonPath("email").value(email))
             .andExpect(jsonPath("name").value(name))
-        ;
+            .andDo(
+                document("member-join",
+                    preprocessRequest(
+                        new PartContentModifyingPreprocessor()
+                    ),
+                    preprocessResponse(
+                        prettyPrint()
+                    ),
+                    requestHeaders(
+                        headerWithName(HttpHeaders.CONTENT_TYPE)
+                            .description("요청 메시지의 콘텐츠 타입 +" + "\n" + MediaType.MULTIPART_FORM_DATA),
+                        headerWithName(HttpHeaders.ACCEPT)
+                            .description("응답받을 콘텐츠 타입 +" + "\n" + MediaType.APPLICATION_JSON)
+                    ),
+                    requestParameters(
+                        parameterWithName("email").description("회원가입에 사용할 이메일 (필수)"),
+                        parameterWithName("password").description("회원가입에 사용할 비빌번호 (필수)"),
+                        parameterWithName("name").description("회원가입에 사용할 이름 (필수)")
+                    ),
+                    requestParts(
+                        partWithName("profileImage").description("프로필 사진으로 사용될 이미지 파일 (필수)")
+                    ),
+                    responseFields(
+                        fieldWithPath("email").description("회원가입이 왼료된 회원의 이메일"),
+                        fieldWithPath("name").description("회원가입이 왼료된 회원의 이름")
+                    )
+                )
+            );
     }
 
     @Test
@@ -107,5 +157,22 @@ class MemberControllerTest {
             .andExpect(jsonPath("status").value(HttpStatus.CONFLICT.value()))
             .andExpect(jsonPath("message").value("Duplicated Email."))
         ;
+    }
+
+    static final class PartContentModifyingPreprocessor extends OperationPreprocessorAdapter {
+
+        private final OperationRequestPartFactory partFactory = new OperationRequestPartFactory();
+        private final OperationRequestFactory requestFactory = new OperationRequestFactory();
+
+        @Override
+        public OperationRequest preprocess(OperationRequest request) {
+            List<OperationRequestPart> parts = new ArrayList<>();
+            for (OperationRequestPart part : request.getParts()) {
+                parts.add(partFactory.create(part.getName(), part.getSubmittedFileName(),
+                    "<< binary data >>".getBytes(), part.getHeaders()));
+            }
+            return requestFactory.create(request.getUri(), request.getMethod(),
+                request.getContent(), request.getHeaders(), request.getParameters(), parts);
+        }
     }
 }
