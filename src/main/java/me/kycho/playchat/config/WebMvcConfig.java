7350e0c3b0643efb0cfdcd87387bcf31042a0e79
khcho902 <khcho902@gmail.com>
2022-03-02 20:56:14 +0900

    feat: #28 cors 허용하는 설정 추가 (#29)

---
+++ src/main/java/me/kycho/playchat/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package me.kycho.playchat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
